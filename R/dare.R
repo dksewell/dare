7671645b4f5537f896a9256d61ec84b3eb400c55
dksewell <dksewell@uiowa.edu>
2025-10-13 15:02:37 -0500

    Added better documentation to dare package

--- R/dare.R
+++ R/dare.R
@@ -5,7 +5,9 @@
 #' @param formula an object of class "formula" (or one that can be coerced to
 #' that class): a symbolic description of the model to be fitted.  Format should be 
 #' <response> ~ <fixed effect 1> + <fixed effect 2> + ... + (<time variable> | <id>)
-#' @param data a data.frame containing the variables in the model.
+#' @param data a data.frame containing the variables in the model.  Each observation,  
+#' positive or negative, should be its own row.  Data will automatically be formatted 
+#' and censored correctly for the analysis.
 #' @param dose_response The dose-response model to be used.  Either the
 #' beta-poisson, exponential.  The simple threshold model is not finished yet.
 #' @param prior_regr_coefs a named list giving the hyperparameters of the
